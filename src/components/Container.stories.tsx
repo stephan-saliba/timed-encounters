d75741551f6196466f7a31c619f8aeb7048ea9b9
Sepehr Ansaripour <pehrans@gmail.com>
2022-08-12 19:46:55 -0400

    ♻️ Use arg-types

--- src/components/Container.stories.tsx
+++ src/components/Container.stories.tsx
@@ -7,29 +7,20 @@ export default {
   parameters: {
     layout: "fullscreen",
   },
+  argTypes: {
+    type: {
+      options: ["normal", "basic", "golden", "golden-2"],
+      control: { type: "radio" },
+    },
+  },
 } as ComponentMeta<typeof Container>;
 
-const Template: ComponentStory<typeof Container> = (args) => <Container {...args} />;
+const Template: ComponentStory<typeof Container> = (args) => (
+  <Container {...args} />
+);
 
-export const Normal = Template.bind({});
-Normal.args = {
+export const Component = Template.bind({});
+Component.args = {
   children: "Hello, World!",
-};
-
-export const Basic = Template.bind({});
-Basic.args = {
-  children: "Hello, World!",
-  type: "basic"
-};
-
-export const Golden = Template.bind({});
-Golden.args = {
-  children: "Hello, World!",
-  type: "golden"
-};
-
-export const Golden2 = Template.bind({});
-Golden2.args = {
-  children: "Hello, World!",
-  type: "golden-2"
+  type: "normal",
 };
